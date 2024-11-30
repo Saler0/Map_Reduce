ef592e6dfa498180213aa55fddd5091f81a0669e
Saler0 <milenko.espinozag@gmail.com>
2024-11-30 13:30:07 +0100

    Featue: añadido map_reduce_grafo con los inputos y outputs esperados

---
+++ map_reduce_grafo.py
@@ -0,0 +1,40 @@
+def map (k,v):
+    return [ (k,([k,k],[v,k])) ]
+
+def reduce (k, lv ):
+    outgoing = []
+    incoming = []
+    for v in lv:
+        if k == v[0]:
+            outgoing.append(v[1])
+        elif k == v[1]:
+            incoming.append(v[0])
+    retValue = []
+    print("outgoing:",outgoing)
+    print("incoming:",incoming)
+    for o in outgoing:
+        for i in incoming:
+            if o != 'A':
+                retValue.append( i+" -2-> "+o )
+            else:
+                retValue.append(o+" -2-> C")
+    return retValue
+
+exampleInput = {'A': 'B', 'B': 'C', 'C': 'D', 'D': 'B'}
+
+print("Input:",exampleInput)
+
+# Fase Mapeo
+resultadosMap = []
+for key,value in exampleInput.items():
+    resultadosMap.append(map(key,value))
+print("Resultados map:",resultadosMap)
+
+# Fase reducción
+resultadosReuce = []
+for resulmap in resultadosMap:
+    tupla=resulmap[0]
+    print("keymap:",tupla[0])
+    print("valuemap:",tupla[1])
+    resultadosReuce.append(reduce(tupla[0],tupla[1]))
+print("Resultados reduce:",resultadosReuce)
